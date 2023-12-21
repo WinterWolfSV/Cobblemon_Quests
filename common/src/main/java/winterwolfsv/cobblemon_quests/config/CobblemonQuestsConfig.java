0058933733513282e965f7490bda3ca8867f8679
WinterWolfSV <what112222@gmail.com>
2023-12-21 13:07:07 +0100

    Make version verification be disabled by default

--- common/src/main/java/winterwolfsv/cobblemon_quests/config/CobblemonQuestsConfig.java
+++ common/src/main/java/winterwolfsv/cobblemon_quests/config/CobblemonQuestsConfig.java
@@ -1,7 +1,10 @@
 package winterwolfsv.cobblemon_quests.config;
 
+import winterwolfsv.cobblemon_quests.CobblemonQuests;
+
 public class CobblemonQuestsConfig implements DefaultConfig {
     // All _only final_ fields will be added to the config file. These fields must be primitive types or Strings.
-    final boolean doVersionVerification = true;
+    final boolean doVersionVerification = false;
     final int versionVerificationTimeoutMillis = 1000;
+    final String configVersion = CobblemonQuests.MOD_VERSION;
 }
