4638af5a481197fb7a06dcca3129d341a9eee6aa
WinterWolfSV <what112222@gmail.com>
2024-01-28 01:22:08 +0100

    Implement bodge to fix bad config system until further notice

--- common/src/main/java/winterwolfsv/cobblemon_quests/config/CobblemonQuestsConfig.java
+++ common/src/main/java/winterwolfsv/cobblemon_quests/config/CobblemonQuestsConfig.java
@@ -2,9 +2,14 @@ package winterwolfsv.cobblemon_quests.config;
 
 import winterwolfsv.cobblemon_quests.CobblemonQuests;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CobblemonQuestsConfig implements DefaultConfig {
     // All _only final_ fields will be added to the config file. These fields must be primitive types or Strings.
     final boolean doVersionVerification = false;
     final int versionVerificationTimeoutMillis = 1000;
     final String configVersion = CobblemonQuests.MOD_VERSION;
+    final List<String> blackListedPokemon = new ArrayList<>();
+    // TODO Fix the error when using a string in the config
 }
