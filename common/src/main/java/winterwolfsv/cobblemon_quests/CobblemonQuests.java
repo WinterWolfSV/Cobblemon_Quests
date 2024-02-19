f825bc127a059e3f87a29f8a5064898d6c4c3163
WinterWolfSV <what112222@gmail.com>
2024-02-19 13:57:34 +0100

    Convert pokeball icon to item

--- common/src/main/java/winterwolfsv/cobblemon_quests/CobblemonQuests.java
+++ common/src/main/java/winterwolfsv/cobblemon_quests/CobblemonQuests.java
@@ -1,5 +1,10 @@
 package winterwolfsv.cobblemon_quests;
 
+import net.minecraft.item.Item;
+import net.minecraft.item.Items;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
 import winterwolfsv.cobblemon_quests.config.CobblemonQuestsConfig;
 import winterwolfsv.cobblemon_quests.config.Config;
 import winterwolfsv.cobblemon_quests.events.FTBCobblemonEventHandler;
@@ -8,21 +13,21 @@ import winterwolfsv.cobblemon_quests.tasks.PokemonTaskTypes;
 import java.nio.file.Path;
 import java.util.logging.Logger;
 
-public class CobblemonQuests
-{
-	public static final String MOD_ID = "cobblemon_quests";
-	public static final String MOD_VERSION = "1.1.8";
-	public static final Logger LOGGER = Logger.getLogger(MOD_ID);
-	public static Path configPath;
-	public static Config config;
-	public static FTBCobblemonEventHandler eventHandler;
+public class CobblemonQuests {
+    public static final String MOD_ID = "cobblemon_quests";
+    public static final String MOD_VERSION = "1.1.8";
+    public static final Logger LOGGER = Logger.getLogger(MOD_ID);
+    public static Path configPath;
+    public static Config config;
+    public static FTBCobblemonEventHandler eventHandler;
 
-	public static void init(Path configPath, boolean useConfig) {
-		if (useConfig) {
-			CobblemonQuests.configPath = configPath;
-			CobblemonQuests.config = new Config(new CobblemonQuestsConfig());
-		}
-		eventHandler = new FTBCobblemonEventHandler().init();
-		PokemonTaskTypes.init();
-	}
+    public static void init(Path configPath, boolean useConfig) {
+        Registry.register(Registries.ITEM, new Identifier(MOD_ID, "poke_ball_icon"), new Item(new Item.Settings()));
+        if (useConfig) {
+            CobblemonQuests.configPath = configPath;
+            CobblemonQuests.config = new Config(new CobblemonQuestsConfig());
+        }
+        eventHandler = new FTBCobblemonEventHandler().init();
+        PokemonTaskTypes.init();
+    }
 }
