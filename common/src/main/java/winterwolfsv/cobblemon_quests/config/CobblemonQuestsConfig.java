bc9be09a7937458956f5edcdac862cf8396a0270
WinterWolfSV <what112222@gmail.com>
2024-02-20 20:58:11 +0100

    No longer uses yaml for config

--- common/src/main/java/winterwolfsv/cobblemon_quests/config/CobblemonQuestsConfig.java
+++ common/src/main/java/winterwolfsv/cobblemon_quests/config/CobblemonQuestsConfig.java
@@ -1,17 +1,14 @@
 package winterwolfsv.cobblemon_quests.config;
 
-import org.yaml.snakeyaml.Yaml;
 import winterwolfsv.cobblemon_quests.CobblemonQuests;
 
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 import java.util.logging.Level;
 
 import static winterwolfsv.cobblemon_quests.CobblemonQuests.LOGGER;
@@ -23,38 +20,52 @@ public class CobblemonQuestsConfig {
     public static boolean suppressWarnings = false;
 
     public static void init() {
-        Yaml yaml = new Yaml();
         File configFile = configPath.toFile();
+        load(configFile);
+    }
+
+    private static void load(File configFile) {
         if (!configFile.exists()) {
-            createDefaultConfig(configFile, yaml);
+            createDefaultConfig(configFile);
             return;
         }
-        Map<String, Object> config = new HashMap<>();
         try {
-            config = yaml.load(Files.newBufferedReader(configPath));
-            if (config == null) throw new Exception();
-        } catch (Exception e) {
-            CobblemonQuests.LOGGER.warning("Failed to load config file for Cobblemon Quests. This is a critical error and may cause crashes.");
-        }
-        try {
-            if ((double) config.get("configVersion") < configVersion) {
-                CobblemonQuests.LOGGER.info("CobblemonQuestsConfig file for Cobblemon Quests is outdated. Attempting to migrate.");
-                // Handle migration here
+            for (String line : Files.readAllLines(configPath)) {
+                String[] split = line.split(": ");
+                if (split.length != 2) {
+                    LOGGER.log(Level.WARNING, "Failed to parse config line: " + line);
+                    continue;
+                }
+                String key = split[0];
+                String value = split[1];
+                switch (key) {
+                    case "configVersion":
+                        if (Double.parseDouble(value) < configVersion)
+                            LOGGER.info("CobblemonQuestsConfig file for Cobblemon Quests is outdated. Attempting to migrate.");
+                        // Handle migration here
+                        break;
+                    case "suppressWarnings":
+                        suppressWarnings = Boolean.parseBoolean(value);
+                        break;
+                    case "ignoredPokemon":
+                        ignoredPokemon = new ArrayList<>(List.of(Arrays.stream(value.split(", ")).map(String::trim).toArray(String[]::new)));
+                        break;
+                    default:
+                        LOGGER.log(Level.WARNING, "Unknown config key: " + key);
+                }
             }
-            ignoredPokemon = (List<String>) config.get("ignoredPokemon");
-            suppressWarnings = (boolean) config.get("suppressWarnings");
         } catch (Exception e) {
-            throw new RuntimeException("Failed to load config values for Cobblemon Quests. Please verify the config file or delete it to generate a new one. " + e);
+            LOGGER.log(Level.WARNING, "Failed to load config file for Cobblemon Quests. This is a critical error and may cause crashes.", e);
         }
+        return;
     }
 
     public static void save() {
-        Yaml yaml = new Yaml();
         File configFile = configPath.toFile();
-        createDefaultConfig(configFile, yaml);
+        createDefaultConfig(configFile);
     }
 
-    private static void createDefaultConfig(File configFile, Yaml yaml) {
+    private static void createDefaultConfig(File configFile) {
         if (!configFile.exists()) {
             try {
                 Files.createDirectories(configPath.getParent());
@@ -66,11 +77,11 @@ public class CobblemonQuestsConfig {
             }
         }
         try {
-            Map<String, Object> defaultConfig = new HashMap<>();
-            defaultConfig.put("configVersion", configVersion);
-            defaultConfig.put("ignoredPokemon", ignoredPokemon);
-            defaultConfig.put("suppressWarnings", suppressWarnings);
-            yaml.dump(defaultConfig, new FileWriter(configFile));
+            List<String> defaultConfig = new ArrayList<>();
+            defaultConfig.add("configVersion: " + configVersion);
+            defaultConfig.add("ignoredPokemon: " + String.join(", ", ignoredPokemon));
+            defaultConfig.add("suppressWarnings: " + suppressWarnings);
+            Files.write(configPath, defaultConfig);
         } catch (Exception e) {
             LOGGER.log(Level.WARNING, "Failed to populate config file for Cobblemon Quests.", e);
         }
