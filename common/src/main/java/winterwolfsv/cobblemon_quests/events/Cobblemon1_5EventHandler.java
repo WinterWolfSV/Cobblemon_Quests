214618d69948e3c2bd236e1739223bd34fb85c86
WinterWolfSV <what112222@gmail.com>
2024-06-15 18:02:54 +0200

    Remove debug print

--- common/src/main/java/winterwolfsv/cobblemon_quests/events/Cobblemon1_5EventHandler.java
+++ common/src/main/java/winterwolfsv/cobblemon_quests/events/Cobblemon1_5EventHandler.java
@@ -12,17 +12,14 @@ import java.util.Arrays;
 
 public class Cobblemon1_5EventHandler {
 
-    public Cobblemon1_5EventHandler init() {
+    public void init() {
         CobblemonEvents.FOSSIL_REVIVED.subscribe(Priority.LOWEST, this::fossilRevived);
-
-        return new Cobblemon1_5EventHandler();
     }
 
     private Unit fossilRevived(FossilRevivedEvent fossilRevivedEvent) {
         try {
             ServerPlayerEntity player = fossilRevivedEvent.getPlayer();
             Pokemon pokemon = fossilRevivedEvent.getPokemon();
-            System.out.println("Fossil Revived Event with player " + player.getName().getString() + " and pokemon " + pokemon.getDisplayName());
             CobblemonQuests.eventHandler.fossilRevivedHandler(player, pokemon);
         } catch (Exception e) {
             CobblemonQuests.LOGGER.warning("Error processing fossil revive event " + Arrays.toString(e.getStackTrace()));
