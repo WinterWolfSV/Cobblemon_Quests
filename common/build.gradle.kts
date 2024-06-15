3f25e237743f0a2c33fb8cfefc89d906eec0cb35
WinterWolfSV <what112222@gmail.com>
2024-06-16 01:58:25 +0200

    Update buildscripts to work with 1.19.2

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -14,18 +14,24 @@ loom.mixin {
 }
 
 dependencies {
-    minecraft("com.mojang:minecraft:1.20.1")
+    minecraft("com.mojang:minecraft:1.19.2")
     mappings("net.fabricmc:yarn:${property("yarn_mappings")}:v2")
-    modCompileOnly("com.cobblemon:mod:1.5.3+1.20.1-SNAPSHOT")
+    // Find this version on https://maven.impactdev.net/#browse/browse:development:com%2Fcobblemon
+    modCompileOnly("com.cobblemon:mod:1.3.0+1.19.2")
 
 
     // alL fabric dependencies:
     modCompileOnly("net.fabricmc:fabric-loader:${property("fabric_loader_version")}")
     modCompileOnly("net.fabricmc.fabric-api:fabric-api:${property("fabric_version")}")
-    modCompileOnly("dev.architectury:architectury-fabric:9.1.12")
-    modCompileOnly("curse.maven:ftb-quests-fabric-438496:4760285")
-    modCompileOnly("curse.maven:ftb-teams-fabric-438497:4623115")
-    modCompileOnly("curse.maven:ftb-library-fabric-438495:4720055")
-    modCompileOnly("curse.maven:item-filters-309674:4838265")
+    //https://www.curseforge.com/minecraft/mc-mods/architectury-api/files
+    modCompileOnly("dev.architectury:architectury-fabric:6.6.92")
+    //https://www.curseforge.com/minecraft/mc-mods/ftb-quests-fabric/files
+    modCompileOnly("curse.maven:ftb-quests-fabric-438496:5417955")
+    //https://www.curseforge.com/minecraft/mc-mods/ftb-teams-fabric/files
+    modCompileOnly("curse.maven:ftb-teams-fabric-438497:4611937")
+    //https://www.curseforge.com/minecraft/mc-mods/ftb-library-fabric/files
+    modCompileOnly("curse.maven:ftb-library-fabric-438495:4661833")
+    //https://www.curseforge.com/minecraft/mc-mods/item-filters/files
+    modCompileOnly("curse.maven:item-filters-309674:4553325")
 
 }
--- fabric/build.gradle.kts
+++ fabric/build.gradle.kts
@@ -28,29 +28,34 @@ loom {
 }
 
 dependencies {
-    minecraft("net.minecraft:minecraft:1.20.1")
+    minecraft("net.minecraft:minecraft:1.19.2")
     mappings("net.fabricmc:yarn:${property("yarn_mappings")}:v2")
-    modApi("dev.architectury:architectury-fabric:9.1.12")
-    modApi("teamreborn:energy:3.0.0")
+    //https://www.curseforge.com/minecraft/mc-mods/architectury-api/files
+    modApi("dev.architectury:architectury-fabric:6.6.92")
+    modApi("teamreborn:energy:2.3.0")
 
 
     modImplementation("net.fabricmc:fabric-loader:${property("fabric_loader_version")}")
     modApi("net.fabricmc.fabric-api:fabric-api:${property("fabric_version")}")
 
 
-    modImplementation(fabricApi.module("fabric-command-api-v2", "0.89.3+1.20.1"))
+    modImplementation(fabricApi.module("fabric-command-api-v2", "0.76.1+1.19.2"))
 
 
     "common"(project(":common", "namedElements")) { isTransitive = false }
     "shadowCommon"(project(":common", "transformProductionFabric")) { isTransitive = false }
 
 
-    modImplementation("com.cobblemon:fabric:1.5.3+1.20.1-SNAPSHOT")
-    modImplementation("curse.maven:ftb-quests-fabric-438496:4760285")
-    modImplementation("curse.maven:ftb-teams-fabric-438497:4623115")
-    modImplementation("curse.maven:ftb-library-fabric-438495:4720055")
-    modImplementation("curse.maven:item-filters-309674:4838265")
-
+    // Find this version on https://maven.impactdev.net/#browse/browse:development:com%2Fcobblemon
+    modImplementation("com.cobblemon:fabric:1.3.0+1.19.2")
+    //https://www.curseforge.com/minecraft/mc-mods/ftb-quests-fabric/files
+    modImplementation("curse.maven:ftb-quests-fabric-438496:5417955")
+    //https://www.curseforge.com/minecraft/mc-mods/ftb-teams-fabric/files
+    modImplementation("curse.maven:ftb-teams-fabric-438497:4611937")
+    //https://www.curseforge.com/minecraft/mc-mods/ftb-library-fabric/files
+    modImplementation("curse.maven:ftb-library-fabric-438495:4661833")
+    //https://www.curseforge.com/minecraft/mc-mods/item-filters/files
+    modImplementation("curse.maven:item-filters-309674:4553325")
 }
 
 tasks.processResources {
