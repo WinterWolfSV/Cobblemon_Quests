f6047288cfc3631e1c2eada5b8272e6f05772dfc
WinterWolfSV <what112222@gmail.com>
2024-12-20 22:12:16 +0100

    Update fabric code

--- fabric/src/main/java/winterwolfsv/cobblemon_quests/fabric/Cobblemon_QuestsFabric.java
+++ fabric/src/main/java/winterwolfsv/cobblemon_quests/fabric/Cobblemon_QuestsFabric.java
@@ -1,10 +1,10 @@
 package winterwolfsv.cobblemon_quests.fabric;
 
 import net.fabricmc.loader.api.FabricLoader;
-import net.minecraft.item.Item;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
-import net.minecraft.util.Identifier;
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
 import winterwolfsv.cobblemon_quests.CobblemonQuests;
 import net.fabricmc.api.ModInitializer;
 import winterwolfsv.cobblemon_quests.fabric.config.ConfigCommandsFabric;
@@ -14,7 +14,8 @@ import static winterwolfsv.cobblemon_quests.CobblemonQuests.MOD_ID;
 public class Cobblemon_QuestsFabric implements ModInitializer {
     @Override
     public void onInitialize() {
-        Registry.register(Registries.ITEM, new Identifier(MOD_ID, "v1.1.11"), new Item(new Item.Settings()));
+        Registry.register(BuiltInRegistries.ITEM, ResourceLocation.fromNamespaceAndPath(MOD_ID, "v1.1.12"), new Item(new Item.Properties()));
+
         CobblemonQuests.init(FabricLoader.getInstance().getConfigDir().resolve(MOD_ID).resolve(MOD_ID + ".config"),true);
         ConfigCommandsFabric.registerCommands();
     }
